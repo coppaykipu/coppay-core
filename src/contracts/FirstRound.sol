130c65c01127f5c98418ffc1f31d9d311dda8823
Matiasjs1 <162461241+Matiasjs1@users.noreply.github.com>
2024-10-18 16:22:47 -0300

    Update FirstRound.sol

--- src/contracts/FirstRound.sol
+++ src/contracts/FirstRound.sol
@@ -1,4 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
+import {IAccessControl} from 'interfaces/IAccessControl.sol';
 import {IFirstRound} from 'interfaces/IFirstRound.sol';
+
+contract FirstRoundModule is IFirstRound {
+  IAccessControl public accessControl;
+
+  constructor(address addressAccessControl) {
+    accessControl = IAccessControl(addressAccessControl);
+  }
+
+  uint256 public constant _PROPOSAL_TIME = 7 days;
+  uint256 public _proposalId;
+  mapping(address _user => mapping(uint256 _proposalId => bool)) userVoted;
+  mapping(uint256 _proposalId => Proposal) proposals;
+  /*
+    modifier OnlyUserRegistered(){
+        if(!accessControl.isRegistered(msg.sender)) revert NotUserRegistered();
+        _;
+    
+    */
+
+  function createProposal(string memory _description, uint256 _budget) public /*OnlyUserRegistered()*/ {
+    if (bytes(_description).length == 0 || _budget == 0) revert paramNotFound();
+    _proposalId++;
+    Proposal memory newProposal = Proposal({
+      idProposal: _proposalId,
+      description: _description,
+      budget: _budget,
+      neededVotes: _neededVotes(),
+      startDate: block.timestamp,
+      totalVotes: 0
+    });
+    proposals[_proposalId] = newProposal;
+    emit proposalCreated(msg.sender, newProposal);
+  }
+
+  function voteProposal(uint256 proposalId) public /*OnlyUserRegistered()*/ {
+    /*if (!accessControl.isRegisteredBefore(msg.sender, proposals[proposalId].startDate)) revert userIsNotRegisteredBefore();*/
+    if (userVoted[msg.sender][proposalId]) revert userAlreadyVoted();
+    userVoted[msg.sender][proposalId] = true;
+    proposals[proposalId].totalVotes++;
+    emit proposalVoted(msg.sender, proposalId, proposals[proposalId].totalVotes);
+  }
+
+  function _neededVotes() private view returns (uint256) {
+    /*
+        return accessControl.getRegisteredUsersCount()/2;
+        */
+    return 1;
+  }
+}
