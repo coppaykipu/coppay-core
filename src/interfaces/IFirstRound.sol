e913334db9dfe90e1a63eb9dcaa7fafc035f72ee
Ashitaka <ashitaka@defi.sucks>
2024-10-18 18:30:23 -0300

    feat: natspec

--- src/interfaces/IFirstRound.sol
+++ src/interfaces/IFirstRound.sol
@@ -1,24 +1,95 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
+/**
+ * @title IFirstRound
+ * @dev IFirstRound interface
+ */
 interface IFirstRound {
-  event proposalCreated(address user, Proposal proposal);
-  event proposalVoted(address user, uint256 proposal, uint256 totalVotes);
+  /*///////////////////////////////////////////////////////////////
+                              STRUCTS
+  //////////////////////////////////////////////////////////////*/
 
-  error userAlreadyVoted();
-  error paramNotFound();
-  error NotUserRegistered();
-  error userIsNotRegisteredBefore();
-
+  /**
+   * @notice Proposal struct
+   * @param proposalID The ID of the proposal
+   * @param description The description of the proposal
+   * @param budget The budget of the proposal
+   * @param neededVotes The needed votes to approve the proposal
+   * @param totalVotes The total votes of the proposal
+   * @param startDate The start date of the proposal
+   */
   struct Proposal {
-    uint256 idProposal;
+    uint256 proposalID;
     string description;
     uint256 budget;
     uint256 neededVotes;
-    uint256 startDate;
     uint256 totalVotes;
+    uint256 startDate;
   }
 
+  /*///////////////////////////////////////////////////////////////
+                              EVENTS
+  //////////////////////////////////////////////////////////////*/
+
+  /**
+   * @notice Event emitted when a proposal is created
+   * @param _user The address of the user
+   * @param _proposal The proposal
+   */
+  event ProposalCreated(address indexed _user, Proposal _proposal);
+
+  /**
+   * @notice Event emitted when a proposal is voted
+   * @param _user The address of the user
+   * @param _proposalID The ID of the proposal
+   * @param _totalVotes The total votes of the proposal
+   */
+  event ProposalVoted(address indexed _user, uint256 indexed _proposalID, uint256 _totalVotes);
+
+  /*///////////////////////////////////////////////////////////////
+                              ERRORS
+  //////////////////////////////////////////////////////////////*/
+
+  /**
+   * @notice Error emitted when a user already voted
+   */
+  error UserAlreadyVoted();
+
+  /**
+   * @notice Error emitted when a user is not registered
+   */
+  error ParamNotFound();
+
+  /**
+   * @notice Error emitted when a user is not registered
+   */
+  error NotUserRegistered();
+
+  /**
+   * @notice Error emitted when a user is not registered
+   */
+  error UserIsNotRegisteredBefore();
+
+  /*///////////////////////////////////////////////////////////////
+                              LOGIC
+  //////////////////////////////////////////////////////////////*/
+
+  /**
+   * @notice Create a proposal
+   * @param _description The description of the proposal
+   * @param _budget The budget of the proposal
+   */
   function createProposal(string memory _description, uint256 _budget) external;
+
+  /**
+   * @notice Vote a proposal
+   * @param proposalId The ID of the proposal
+   */
   function voteProposal(uint256 proposalId) external;
+
+  /**
+   * @notice Finalize proposals
+   */
+  function finalizeProposals() external;
 }
