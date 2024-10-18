b2a1443a3377657b7e7a3b39a4e719ccb196847c
Ashitaka <96790496+ashitakah@users.noreply.github.com>
2024-10-18 18:53:16 -0300

    feat: second round and vault (#3)

--- src/interfaces/IVault.sol
+++ src/interfaces/IVault.sol
@@ -1,4 +1,72 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-interface IVault {}
+interface IVault {
+  /*///////////////////////////////////////////////////////////////
+                              STRUCTS
+  //////////////////////////////////////////////////////////////*/
+
+  /*///////////////////////////////////////////////////////////////
+                              EVENTS
+  //////////////////////////////////////////////////////////////*/
+
+  /**
+   * @notice Event emitted when a budget is locked
+   * @param _amount The amount locked
+   */
+  event BudgetLocked(uint256 _amount);
+
+  /**
+   * @notice Event emitted when a budget is unlocked
+   * @param _amount The amount unlocked
+   */
+  event BudgetUnlocked(uint256 _amount);
+
+  /**
+   * @notice Event emitted when a budget is updated
+   * @param _amount The amount updated
+   */
+  event BudgetUpdated(uint256 _amount);
+
+  /*///////////////////////////////////////////////////////////////
+                              ERRORS
+  //////////////////////////////////////////////////////////////*/
+
+  /**
+   * @notice Error emitted when a user does not have enough votes
+   */
+  error InsufficientVotes();
+
+  /*///////////////////////////////////////////////////////////////
+                              VIEWS
+  //////////////////////////////////////////////////////////////*/
+
+  /**
+   * @notice Check if a user has enough votes
+   * @param _user The address of the user
+   * @param _numVotes The number of votes
+   */
+  function haveVotes(address _user, uint256 _numVotes) external view;
+
+  /*///////////////////////////////////////////////////////////////
+                              LOGIC
+  //////////////////////////////////////////////////////////////*/
+
+  /**
+   * @notice Lock a budget for a proposal that has been accepted
+   * @param _amount The amount to lock
+   */
+  function lockBudget(uint256 _amount) external;
+
+  /**
+   * @notice Unlock a budget for a proposal that has been cancelled
+   * @param _amount The amount to unlock
+   */
+  function unlockBudget(uint256 _amount) external;
+
+  /**
+   * @notice Update the budget
+   * @param _amount The amount to update
+   */
+  function updateBudget(uint256 _amount) external;
+}
