520e73e946bd0a3463cd9fea6fa158677594da88
rodripettinato <111130262+rodripettinato@users.noreply.github.com>
2024-10-18 15:23:46 -0300

    Update IAccessControl.sol
    Se añadieron la Interfaces.

--- src/interfaces/IAccessControl.sol
+++ src/interfaces/IAccessControl.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-interface IAccessControl {}
+interface IAccessControl {
+    function isRegistered(address user) external view returns (bool);
+    function isRegisteredBefore(address user, uint256 timestamp) external view returns (bool);
+    function getRegisteredUsersCount() external view returns (uint256);
+    function isAdmin(address user) external view returns (bool);
+    function submit(address user, uint256) external;
+    function approveUser(address user) external;
+    function disapproveUser(address user) external;
+}
