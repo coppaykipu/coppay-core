6e085c326571cff4c0eec4e00f9c7b7d5e720b73
nicoacosta.eth <nicoacosta8@gmail.com>
2024-11-01 14:50:01 -0300

    feat: add deploy challenge template

---
+++ script/DeployChallenge.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Script} from 'forge-std/Script.sol';
+import {console} from 'forge-std/console.sol';
+
+contract DeployChallenge is Script {
+  function run() public {}
+}
